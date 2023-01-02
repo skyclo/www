411f608bf47ee7d076433abd1c4f80ea41359d43
Skyclo <hello.skyclo@gmail.com>
2023-01-02 17:29:11 -0500

    🎨 add prettier

--- app/root.jsx
+++ app/root.jsx
@@ -1,38 +1,29 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react"
 
 // TailwindCSS
-import styles from "app/styles/tailwind.css";
+import styles from "app/styles/tailwind.css"
 
-export const links = () => ([
-  { rel: "stylesheet", href: styles },
-])
+export const links = () => [{ rel: "stylesheet", href: styles }]
 
 export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
+})
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    )
 }
