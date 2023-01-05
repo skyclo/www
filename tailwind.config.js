6b3202f7598a3399ad681328acd9173800e8919e
Skyclo <hello.skyclo@gmail.com>
2023-01-05 00:14:31 -0500

    💄 add Gilroy font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
     content: ["./app/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        fontFamily: {
+            sans: [
+                "Gilroy",
+                "ui-sans-serif",
+                "system-ui",
+                "-apple-system",
+                "BlinkMacSystemFont",
+                "Segoe UI",
+                "Roboto",
+                "Helvetica Neue",
+                "Arial",
+                "Noto Sans",
+                "sans-serif",
+                "Apple Color Emoji",
+                "Segoe UI Emoji",
+                "Segoe UI Symbol",
+                "Noto Color Emoji",
+            ],
+        },
         extend: {},
     },
     plugins: [],
