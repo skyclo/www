a515266dce7cf28d6b1b5be0b58df2fe851e16dd
Skyclo <hello.skyclo@gmail.com>
2023-01-05 00:58:30 -0500

    🍱 add favicons

--- app/root.jsx
+++ app/root.jsx
@@ -7,8 +7,11 @@ export const links = () => [{ rel: "stylesheet", href: styles }]
 
 export const meta = () => ({
     charset: "utf-8",
-    title: "New Remix App",
+    title: "Conor Daly - skyclo.dev",
     viewport: "width=device-width,initial-scale=1",
+
+    "msapplication-TileColor": "#da532c",
+    "theme-color": "#ffffff",
 })
 
 export default function App() {
@@ -17,6 +20,10 @@ export default function App() {
             <head>
                 <Meta />
                 <Links />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </head>
             <body>
                 <Outlet />
