955eecb1697bc3edf798e6b641586c24342c465d
Skyclo <hello.skyclo@gmail.com>
2023-01-08 22:07:59 -0500

    🔍 add meta tags, og embed info

--- app/root.jsx
+++ app/root.jsx
@@ -6,13 +6,46 @@ import styles from "app/styles/app.css"
 export const links = () => [{ rel: "stylesheet", href: styles }]
 
 export const meta = () => ({
-    charset: "utf-8",
     title: "Conor Daly - skyclo.dev",
+    description: "Conor's interactive, online portfolio.",
+    keywords: "conor,daly,website,portfolio,resume,software,technology,programming,coding,skyclo",
+    author: "Conor Daly",
+    image: "/images/og_embed.png",
+
+    charset: "utf-8",
     viewport: "width=device-width,initial-scale=1",
+    language: "en-us",
+    robots: "index, follow",
+
+    "twitter:card": "summary_large_image",
+    "twitter:url": "https://skyclo.dev",
+    "twitter:title": "Conor Daly - skyclo.dev",
+    "twitter:description": "Conor's interactive, online portfolio.",
+    "twitter:image": "/images/og_embed.png",
+
+    "og:type": "website",
+    "og:url": "https://skyclo.dev",
+    "og:title": "Conor Daly - skyclo.dev",
+    "og:description": "Conor's interactive, online portfolio.",
+    "og:image": "/images/og_embed.png",
 
     "msapplication-TileColor": "#da532c",
     "theme-color": "#ffffff",
 })
+
+const schema = {
+    "@context": "https://schema.org/",
+    "@type": "Person",
+    name: "Conor Daly",
+    url: "https://skyclo.dev/",
+    image: "",
+    sameAs: [
+        "https://twitter.com/skyclo_",
+        "https://linkedin.com/in/skyclo",
+        "https://github.com/skyclo",
+        "https://skyclo.dev/",
+    ],
+}
 
 export default function App() {
     return (
@@ -24,6 +57,13 @@ export default function App() {
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
+                <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify(schema),
+                    }}
+                />
             </head>
             <body>
                 <Outlet />
