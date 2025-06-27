ff33b0db23b1238ad6cee2017a1c9c26ba171e2f
skyclo <hello.skyclo@gmail.com>
2025-06-27 17:32:02 -0400

    🐛 fix nav hover, add new site info bar

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,4 @@
+import { InfoIcon } from "lucide-react"
 import { useEffect, useRef, useState } from "react"
 import rx from "~/components/rx"
 
@@ -59,23 +60,29 @@ export default function Index() {
 
     return (
         <>
+            <div className="absolute top-0 left-0 z-10 flex w-screen flex-row items-center bg-blue-500 px-4 py-2 font-bold text-white">
+                <div className="mx-auto flex flex-row items-center space-x-2">
+                    <InfoIcon className="h-4 w-4" />
+                    <p>New site coming soon</p>
+                </div>
+            </div>
             <nav
-                className={
-                    "absolute top-0 right-0 hidden h-full flex-col md:flex " +
-                    (navVisible ? "opacity-100" : "opacity-0")
-                }>
-                <div
-                    className="my-auto flex flex-col space-y-4 bg-[radial-gradient(at_right_center,_theme(colors.purple.500/28%)_0%,_#00000000_65%)] px-8 py-28"
-                    onMouseOver={() => {
-                        clearTimeout(timeout.current)
-                        setNavVisible(true)
-                    }}
-                    onMouseLeave={() => {
-                        if (!navVisible) return
+                className="fixed top-0 right-0 hidden h-full flex-col md:flex"
+                onMouseOver={() => {
+                    clearTimeout(timeout.current)
+                    setNavVisible(true)
+                }}
+                onMouseLeave={() => {
+                    if (!navVisible) return
 
-                        clearTimeout(timeout.current)
-                        timeout.current = setTimeout(() => setNavVisible(false), 2000)
-                    }}>
+                    clearTimeout(timeout.current)
+                    timeout.current = setTimeout(() => setNavVisible(false), 2000)
+                }}>
+                <div
+                    className={
+                        "my-auto flex flex-col space-y-4 bg-[radial-gradient(at_right_center,_theme(colors.purple.500/28%)_0%,_#00000000_65%)] px-8 py-28 " +
+                        (navVisible ? "opacity-100" : "opacity-0")
+                    }>
                     {rx.map((section, i) => (
                         <div
                             key={i}
